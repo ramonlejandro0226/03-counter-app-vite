31cbc57bc60144b299c9d61d3b437eba653c18cd
ramon alejandro <ramonvelez370@gmail.com>
2024-08-11 15:41:35 -0400

    nothing

--- src/base-pruebas/base-pruebas/11-async-await.js
+++ src/base-pruebas/base-pruebas/11-async-await.js
@@ -4,7 +4,7 @@ export const getImagen = async() => {
 
     try {
 
-        const apiKey = 'xk79sT21506CHoZcDHMkf7VLs3TG0oTP';
+        // const apiKey = 'xk79sT21506CHoZcDHMkf7VLs3TG0oTP';
         const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
         const { data } = await resp.json(); 
 
